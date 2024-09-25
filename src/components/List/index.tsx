7618df891bbf58c80c6571e68dc93804ded7c1ab
Jason <130726285+jsnli@users.noreply.github.com>
2024-09-25 14:14:05 -0400

    adjust list state to reflect checkbox changes

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,36 +1,60 @@
+import { useState, useEffect } from "react";
 import "./index.css";
 
 interface Achievement {
-    api_name: string;
-    name: string;
-    desc: string;
-    status: boolean;
+	api_name: string;
+	name: string;
+	desc: string;
+	status: boolean;
 }
 
 interface ListProps {
-    items: Achievement[];
+	achievements: Achievement[];
 }
 
-function List({ items }: ListProps) {
-    return (
-        <>
-            <h1>List:</h1>
-            <ul className="list">
-                {items.map((item, index) => (
-                    <li key={index}>
-                        <label>
-                            <input type="checkbox" checked={item.status} />
-                            <div>
-                                <span className="name">{item.name}</span>
-                                <span className="desc">{item.desc}</span>
-                            </div>
-                        </label>
+function List({ achievements }: ListProps) {
+	const [items, setItems] = useState<Achievement[]>(achievements);
 
-                    </li>
-                ))}
-            </ul>
-        </>
-    )
+	function testfn() {
+		setItems(achievements);
+		console.log("ach: ", achievements);
+		console.log("items: ", items);
+	}
+
+	useEffect(() => {
+		setItems(achievements);
+	}, [achievements]);
+
+	function handleCheckbox( index: number ) {
+		const newItems = [...items];
+		newItems[index].status = !(items[index].status);
+		setItems(newItems);
+	}
+
+	return (
+		<>
+			<button onClick={testfn}>Apply Changes</button>
+			<ul className="list">
+				{items.map((item, index) => (
+					<li key={index}>
+						<label>
+							<input
+								type="checkbox"
+								checked={item.status}
+								onChange={(event) => handleCheckbox(index)}
+							/>
+							<div>
+								<span className="name">
+									{index} - {item.name} - {item.status}
+								</span>
+								<span className="desc">{item.desc}</span>
+							</div>
+						</label>
+					</li>
+				))}
+			</ul>
+		</>
+	);
 }
 
 export default List;
