507ebe4b9405547d85ea8cdcabee84a92edfa3ca
Jason <130726285+jsnli@users.noreply.github.com>
2024-10-01 17:54:51 -0400

    Resolve panics from schema reading failures
    Signed-off-by: Jason <130726285+jsnli@users.noreply.github.com>

--- src-tauri/src/steam.rs
+++ src-tauri/src/steam.rs
@@ -22,42 +22,45 @@ pub fn start_client(appid: u32) -> Result<Client<ClientManager>, String> {
     match result {
         Ok(client) => Ok(client),
         Err(panic_error) => Err(format!("Panic occured: {:?}", panic_error)),
-        
     }
-
-    
-
 }
 
-pub fn load_achievements(client: Client<ClientManager>) -> Vec<Achievement> {
-    let id: String = client.user().steam_id().raw().to_string();
-    println!("get info. id: {}", id);
+pub fn load_achievements(client: Client<ClientManager>) -> Result<Vec<Achievement>, String> {
+    let result = panic::catch_unwind(AssertUnwindSafe(|| {
+        let id: String = client.user().steam_id().raw().to_string();
+        println!("get info. id: {}", id);
 
-    let user_stats = client.user_stats();
+        let user_stats = client.user_stats();
 
-    let mut AchievementList: Vec<Achievement> = Vec::new();
-    let names = user_stats
-        .get_achievement_names()
-        .expect("Failed to get names");
-    for name in names {
-        let achievement_helper = user_stats.achievement(&name);
-        let a: Achievement = Achievement {
-            api_name: name.clone(),
-            name: achievement_helper
-                .get_achievement_display_attribute("name")
-                .unwrap()
-                .to_string(),
-            desc: achievement_helper
-                .get_achievement_display_attribute("desc")
-                .unwrap()
-                .to_string(),
-            status: achievement_helper.get().unwrap(),
-        };
-        AchievementList.push(a);
-        println!("{}", name);
+        let mut AchievementList: Vec<Achievement> = Vec::new();
+        let names = user_stats
+            .get_achievement_names()
+            .expect("Failed to get names");
+        for name in names {
+            let achievement_helper = user_stats.achievement(&name);
+            let a: Achievement = Achievement {
+                api_name: name.clone(),
+                name: achievement_helper
+                    .get_achievement_display_attribute("name")
+                    .unwrap()
+                    .to_string(),
+                desc: achievement_helper
+                    .get_achievement_display_attribute("desc")
+                    .unwrap()
+                    .to_string(),
+                status: achievement_helper.get().unwrap(),
+            };
+            AchievementList.push(a);
+            println!("{}", name);
+        }
+
+        AchievementList
+    }));
+
+    match result {
+        Ok(list) => Ok(list),
+        Err(panic_error) => Err(format!("Panic occured: {:?}", panic_error)),
     }
-
-    AchievementList
 }
 
 pub fn commit_achievement(client: Client<ClientManager>, name: String, unlocked: bool) {
@@ -74,4 +77,10 @@ pub fn commit_achievement(client: Client<ClientManager>, name: String, unlocked:
 pub fn store_stats(client: Client<ClientManager>) {
     let user_stats = client.user_stats();
     let _ = user_stats.store_stats();
+}
+
+pub fn load_statistics(appid: u32) -> Vec<String> {
+    println!("loading stats");
+    let s: Vec<String> = Vec::new();
+    s
 }
