558d1707fc39a06f5248c1fcbdbd807e2dd6f110
Jason <130726285+jsnli@users.noreply.github.com>
2024-10-18 20:51:41 -0400

    add minimal statistics styling
    Signed-off-by: Jason <130726285+jsnli@users.noreply.github.com>

--- src/components/StatisticView/index.tsx
+++ src/components/StatisticView/index.tsx
@@ -1,12 +1,31 @@
+import { useState, useEffect } from "react";
 import { Stat } from "../../interfaces";
+import "./index.css";
 
-function StatisticView() {
+interface StatisticViewProps {
+	stats: Stat[];
+}
+
+function StatisticView({ stats }: StatisticViewProps) {
+	const [items, setItems] = useState<Stat[]>([]);
+
+	useEffect(() => {
+		const statsClone = structuredClone(stats);
+		setItems(statsClone);
+	}, [stats]);
 
 
 	return (
-		<>
-			<p>Statistics</p>
-		</>
+		<div className="statistic-view">
+			<ul>
+				{items.map((item, index) => (
+					<li key={index}>
+						<label>{item.api_name}</label>
+						<input value={item.value} />
+					</li>
+				))}
+			</ul>
+		</div>
 	)
 
 }
