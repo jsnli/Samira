6f848dfee1e633429c9280db0d52600b1c32800d
Jason <130726285+jsnli@users.noreply.github.com>
2024-10-20 11:34:49 -0400

    Move view toggles to tabs component in sidebar
    Signed-off-by: Jason <130726285+jsnli@users.noreply.github.com>

--- src/components/Status/index.tsx
+++ src/components/Status/index.tsx
@@ -11,22 +11,24 @@ function Status({ message, info }: StatusProps) {
 		<div className="status">
 			<span>Information:</span>	
 			<table>
-				<tr>
-					<td>Game</td>
-					<td>{info.app_name}</td>
-				</tr>
-				<tr>
-					<td>App Id</td>
-					<td>{info.app_id !== 0 ? info.app_id : null}</td>
-				</tr>
-				<tr>
-					<td>Name</td>
-					<td>{info.user_name}</td>
-				</tr>
-				<tr>
-					<td>Steam ID</td>
-					<td>{info.user_id !== 0 ? info.user_id : null}</td>
-				</tr>
+				<tbody>
+					<tr>
+						<td>Game</td>
+						<td>{info.app_name}</td>
+					</tr>
+					<tr>
+						<td>App Id</td>
+						<td>{info.app_id !== 0 ? info.app_id : null}</td>
+					</tr>
+					<tr>
+						<td>Name</td>
+						<td>{info.user_name}</td>
+					</tr>
+					<tr>
+						<td>Steam ID</td>
+						<td>{info.user_id !== 0 ? info.user_id : null}</td>
+					</tr>
+				</tbody>
 			</table>
 
 			<span>Status:</span>
