e7fba02b63a7b4ea86bd45e37f8ad0314ee9823e
Jason <130726285+jsnli@users.noreply.github.com>
2024-10-22 20:06:19 -0400

    Clean and update layout
    Signed-off-by: Jason <130726285+jsnli@users.noreply.github.com>

--- src/components/Status/index.tsx
+++ src/components/Status/index.tsx
@@ -9,30 +9,25 @@ interface StatusProps {
 function Status({ message, info }: StatusProps) {
 	return (
 		<div className="status">
-			<span>Information:</span>	
-			<table>
-				<tbody>
-					<tr>
-						<td>Game</td>
-						<td>{info.app_name}</td>
-					</tr>
-					<tr>
-						<td>App Id</td>
-						<td>{info.app_id !== 0 ? info.app_id : null}</td>
-					</tr>
-					<tr>
-						<td>Name</td>
-						<td>{info.user_name}</td>
-					</tr>
-					<tr>
-						<td>Steam ID</td>
-						<td>{info.user_id !== 0 ? info.user_id : null}</td>
-					</tr>
-				</tbody>
-			</table>
-
-			<span>Status:</span>
-			<div className="status-box">{message}</div>
+			<span className="header">Information:</span>	
+			<div className="info">
+				<label>Game:</label>
+				<span>{info.app_name}</span>
+			</div>
+			<div className="info">
+				<label>AppID:</label>
+				<span>{info.app_id !== 0 ? info.app_id : null}</span>
+			</div>
+			<div className="info">
+				<label>Name:</label>
+				<span>{info.user_name}</span>
+			</div>
+			<div className="info">
+				<label>SteamID:</label>
+				<span>{info.user_id !== 0 ? info.user_id : null}</span>
+			</div>
+			<span className="header">Status:</span>
+			<p>{message}</p>
 		</div>
 	);
 }
