fc0f96eb39c0c2f1a31c1f620a0caef3b41f5a18
Jason <130726285+jsnli@users.noreply.github.com>
2024-11-12 18:40:18 -0500

    add message for games with no statistics

--- src/components/StatisticView/index.tsx
+++ src/components/StatisticView/index.tsx
@@ -4,83 +4,88 @@ import "./index.css";
 import { invoke } from "@tauri-apps/api/tauri";
 
 interface StatisticViewProps {
-  stats: Stat[];
+	stats: Stat[];
 	setAlert: (message: string[]) => void;
 	refresh: () => void;
-
 }
 
 function StatisticView({ stats, setAlert, refresh }: StatisticViewProps) {
-  const [items, setItems] = useState<Stat[]>([]);
+	const [items, setItems] = useState<Stat[]>([]);
 
-  useEffect(() => {
-    const statsClone = structuredClone(stats);
-    setItems(statsClone);
-  }, [stats]);
+	useEffect(() => {
+		const statsClone = structuredClone(stats);
+		setItems(statsClone);
+	}, [stats]);
 
-  function apply() {
-   	const alerts: string[] = []; 
+	function apply() {
+		const alerts: string[] = [];
 
 		const inputs: HTMLInputElement[] = Array.from(
-      document.querySelectorAll(".statistic-view ul li input"),
-    );
-    const values: number[] = inputs.map((input: HTMLInputElement) =>
-      parseFloat(input.value),
-    );
+			document.querySelectorAll(".statistic-view ul li input"),
+		);
+		const values: number[] = inputs.map((input: HTMLInputElement) =>
+			parseFloat(input.value),
+		);
 
-    for (let i = 0; i < items.length; i++) {
+		for (let i = 0; i < items.length; i++) {
 			if (items[i].value == values[i]) {
 				continue;
 			}
-      invoke("cmd_commit_statistics", {
-        name: items[i].api_name,
-        value: values[i],
-      }).then(() => {
-				alerts.push(`${items[i].name} set to ${values[i]} `)
-        console.log(`committing stats - ${items[i].api_name}`);
-      });
-    }
+			invoke("cmd_commit_statistics", {
+				name: items[i].api_name,
+				value: values[i],
+			}).then(() => {
+				alerts.push(`${items[i].name} set to ${values[i]} `);
+				console.log(`committing stats - ${items[i].api_name}`);
+			});
+		}
 
-    invoke("cmd_store_stats").then(() => {
+		invoke("cmd_store_stats").then(() => {
 			setAlert(alerts);
 			refresh();
 		});
-  }
+	}
 
 	function resetDefaultValues() {
 		refresh();
-		const inputs = document.querySelectorAll<HTMLInputElement>(".statistic-view ul li input");
+		const inputs = document.querySelectorAll<HTMLInputElement>(
+			".statistic-view ul li input",
+		);
 		for (let i = 0; i < inputs.length; i++) {
 			inputs[i].value = items[i].value.toString();
-		}	
+		}
 	}
 
-  return (
-    <div className="statistic-view">
-      <ul>
-        {items.map((item, index) => (
-          <li key={index}>
-            <label>{item.name}</label>
-            <input
-              type="number"
-              min={item.min}
-              max={item.max}
-              defaultValue={item.value}
-            />
-            <span>
-              {item.min} - {item.max}
-            </span>
-          </li>
-        ))}
-      </ul>
-      <div className="nav">
-        <button className="apply" onClick={apply}>
-          Apply
-        </button>
-        <button onClick={resetDefaultValues}>Refresh</button>
-      </div>
-    </div>
-  );
+	return (
+		<div className="statistic-view">
+			{items.length > 0 ? (
+				<ul>
+					{items.map((item, index) => (
+						<li key={index}>
+							<label>{item.name}</label>
+							<input
+								type="number"
+								min={item.min}
+								max={item.max}
+								defaultValue={item.value}
+							/>
+							<span>
+								{item.min} - {item.max}
+							</span>
+						</li>
+					))}
+				</ul>
+			) : (
+				<span className="no-statistics">No statistics were found for this game.</span>
+			)}
+			<div className="nav">
+				<button className="apply" onClick={apply}>
+					Apply
+				</button>
+				<button onClick={resetDefaultValues}>Refresh</button>
+			</div>
+		</div>
+	);
 }
 
 export default StatisticView;
