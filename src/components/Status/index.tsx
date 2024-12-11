69c0cd2913bf57a4e1425a25d0166cb24b88762d
Jason <130726285+jsnli@users.noreply.github.com>
2024-12-10 23:54:15 -0500

    Add header image

--- src/components/Status/index.tsx
+++ src/components/Status/index.tsx
@@ -2,40 +2,49 @@ import "./index.css";
 import { Info } from "../../interfaces";
 
 interface StatusProps {
-	message: string[];
-	info: Info;
+  message: string[];
+  info: Info;
 }
 
 function Status({ message, info }: StatusProps) {
-	return (
-		<div className="status">
-			<span className="header">Information:</span>	
-			<div className="info">
-				<label>Game:</label>
-				<span>{info.app_name}</span>
-			</div>
-			<div className="info">
-				<label>AppID:</label>
-				<span>{info.app_id !== 0 ? info.app_id : null}</span>
-			</div>
-			<div className="info">
-				<label>Name:</label>
-				<span>{info.user_name}</span>
-			</div>
-			<div className="info">
-				<label>SteamID:</label>
-				<span>{info.user_id !== 0 ? info.user_id : null}</span>
-			</div>
-			<div className="log">
-				<span>Activity:</span>	
-				<ul>		
-					{message.map((element, index) => (
-						<li key={index}>{element}</li>
-					))}
-				</ul>
-			</div>
-		</div>
-	);
+  return (
+    <div className="status">
+      {info.app_id > 0 ? (
+        <img
+          src={
+            "https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/" +
+            info.app_id +
+            "/header.jpg"
+          }
+        />
+      ) : null}
+      <span className="header">Information:</span>
+      <div className="info">
+        <label>Game:</label>
+        <span>{info.app_name}</span>
+      </div>
+      <div className="info">
+        <label>AppID:</label>
+        <span>{info.app_id !== 0 ? info.app_id : null}</span>
+      </div>
+      <div className="info">
+        <label>Name:</label>
+        <span>{info.user_name}</span>
+      </div>
+      <div className="info">
+        <label>SteamID:</label>
+        <span>{info.user_id !== 0 ? info.user_id : null}</span>
+      </div>
+      <div className="log">
+        <span>Activity:</span>
+        <ul>
+          {message.map((element, index) => (
+            <li key={index}>{element}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Status;
