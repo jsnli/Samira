aa3ec34ac953fe3c91b71693f38dc9b6ffd8f397
Jason <130726285+jsnli@users.noreply.github.com>
2024-12-18 16:35:50 -0500

    Track status updates in groups instead of individually

--- src/App.tsx
+++ src/App.tsx
@@ -11,140 +11,150 @@ import { Achievement, Stat, Info } from "./interfaces";
 import "./App.css";
 
 function App() {
-  const isInitialized = useRef(false);
-  const [achievements, setAchievements] = useState<Achievement[]>([]);
-	const [icons, setIcons] = useState<{[key: string]: string}>({});
-  const [stats, setStats] = useState<Stat[]>([]);
-  const [status, setStatus] = useState<string[]>([]);
-  const [info, setInfo] = useState<Info>({
-    app_id: 0,
-    app_name: "",
-    user_id: 0,
-    user_name: "",
-  });
-  const [databaseReady, setDatabaseReady] = useState<boolean>(false);
+	const isInitialized = useRef(false);
+	const [achievements, setAchievements] = useState<Achievement[]>([]);
+	const [icons, setIcons] = useState<{ [key: string]: string }>({});
+	const [stats, setStats] = useState<Stat[]>([]);
+	const [status, setStatus] = useState<[string, boolean][]>([]);
+	const [info, setInfo] = useState<Info>({
+		app_id: 0,
+		app_name: "",
+		user_id: 0,
+		user_name: "",
+	});
+	const [databaseReady, setDatabaseReady] = useState<boolean>(false);
 
-  const [view, setView] = useState<"a" | "s">("a");
+	const [view, setView] = useState<"a" | "s">("a");
 
-  useEffect(() => {
-    if (isInitialized.current) {
-      return;
-    }
-    isInitialized.current = true;
-    updateStatus("Loading database.");
-    invoke("cmd_request_data").then((response) => {
-      invoke("cmd_populate_data", { apps: response }).then(() => {
-        updateStatus("Database ready.");
-        setDatabaseReady(true);
-      });
-    });
-  }, []);
+	useEffect(() => {
+		if (isInitialized.current) {
+			return;
+		}
+		isInitialized.current = true;
+		updateStatus("Loading database.");
+		invoke("cmd_request_data").then((response) => {
+			invoke("cmd_populate_data", { apps: response }).then(() => {
+				updateStatus("Database ready.");
+				setDatabaseReady(true);
+			});
+		});
+	}, []);
 
-  function updateStatus(input: string | string[]) {
-    if (typeof input === "string") {
-      setStatus((prevStatus) => [input, ...prevStatus]);
-    } else if (Array.isArray(input)) {
-      setStatus((prevStatus) => [...input, ...prevStatus]);
-    }
-  }
+	function updateStatus(input: string | string[]) {
 
-  function selectView(view: string) {
-    if (view == "a" || view == "s") {
-      setView(view);
-    }
-  }
 
-  function handleAppSelection(newID: number, newName: string) {
-    if (newID > 0) {
-      invoke("cmd_start_client", { appid: newID }).then((response) => {
-        if (response) {
-          updateStatus("Starting client.");
-          LoadAchievements();
+		if (typeof input === "string") {
+			setStatus((prev) => [
+				[input, true],
+				...prev.map(([message, _]) => [message, false] as [string, boolean])
+			]);
+			// setStatus((prevStatus) => [input, ...prevStatus]);
+		} else if (Array.isArray(input)) {
+			setStatus((prev) => [
+				...input.map(message => [message,true] as [string, boolean]),
+				...prev.map(([message, _]) => [message, false] as [string, boolean])
+			]);
+			// setStatus((prevStatus) => [...input, ...prevStatus]);
+		}
+	}
+
+	function selectView(view: string) {
+		if (view == "a" || view == "s") {
+			setView(view);
+		}
+	}
+
+	function handleAppSelection(newID: number, newName: string) {
+		if (newID > 0) {
+			invoke("cmd_start_client", { appid: newID }).then((response) => {
+				if (response) {
+					updateStatus("Starting client.");
+					LoadAchievements();
 					LoadAchievementIcons(newID);
-          LoadStatistics(newID);
-          UpdateStatusInfo(newID, newName);
-        } else {
-          updateStatus(
-            "Error loading client. Steam must be running and you must own the game selected.",
-          );
-        }
-      });
-    }
-  }
+					LoadStatistics(newID);
+					UpdateStatusInfo(newID, newName);
+				} else {
+					updateStatus(
+						"Error loading client. Steam must be running and you must own the game selected.",
+					);
+				}
+			});
+		}
+	}
 
-  function LoadAchievements() {
-    invoke("cmd_load_achievements").then((response) => {
-      const data = response as Achievement[];
-      setAchievements(data);
-      updateStatus("Achievements loaded.");
-    });
-  }
+	function LoadAchievements() {
+		invoke("cmd_load_achievements").then((response) => {
+			const data = response as Achievement[];
+			setAchievements(data);
+			updateStatus("Achievements loaded.");
+		});
+	}
 
-  function LoadAchievementIcons(newID: number) {
-    invoke("cmd_load_achievement_icons", { appid: newID }).then((response) => {
-   		const data = response as { [key: string]: string }; 
+	function LoadAchievementIcons(newID: number) {
+		invoke("cmd_load_achievement_icons", { appid: newID }).then((response) => {
+			const data = response as { [key: string]: string };
 			setIcons(data);
 		});
-  }
+	}
 
-  function LoadStatistics(newID: number) {
-    invoke("cmd_load_statistics", { appid: newID }).then((response) => {
-      const data = response as Stat[];
-      setStats(data);
-      updateStatus("Statistics loaded.");
-    });
-  }
+	function LoadStatistics(newID: number) {
+		invoke("cmd_load_statistics", { appid: newID }).then((response) => {
+			const data = response as Stat[];
+			setStats(data);
+			updateStatus("Statistics loaded.");
+		});
+	}
 
-  function UpdateStatusInfo(appid: number, appname: string) {
-    invoke("cmd_retrieve_user").then((response) => {
-      interface User {
-        user_steam_id: number;
-        user_name: string;
-      }
-      const user_data = response as User;
-      setInfo({
-        app_id: appid,
-        app_name: appname,
-        user_id: user_data.user_steam_id,
-        user_name: user_data.user_name,
-      });
-    });
-  }
+	function UpdateStatusInfo(appid: number, appname: string) {
+		invoke("cmd_retrieve_user").then((response) => {
+			interface User {
+				user_steam_id: number;
+				user_name: string;
+			}
+			const user_data = response as User;
+			setInfo({
+				app_id: appid,
+				app_name: appname,
+				user_id: user_data.user_steam_id,
+				user_name: user_data.user_name,
+			});
+		});
+	}
 
-  return (
-    <>
-      <div className="sidebar">
-        <Search
-          onAppSelection={handleAppSelection}
-          databaseReady={databaseReady}
-        />
-        <Tabs handleSelectView={selectView} />
-        <Status message={status} info={info} />
-      </div>
-      <div className="main">
-        {info.app_id == 0 ? (
-          <div className="instructions">
-            Search for a game and select to get started
-          </div>
-        ) : null}
-        {view == "a" && info.app_id != 0 ? (
-          <AchievementView
-            achievements={achievements}
+	return (
+		<>
+			<div className="sidebar">
+				<Search
+					onAppSelection={handleAppSelection}
+					databaseReady={databaseReady}
+				/>
+				<Tabs handleSelectView={selectView} />
+				<Status message={status} info={info} />
+			</div>
+			<div className="main">
+				{info.app_id == 0 ? (
+					<div className="instructions">
+						Search for a game and select to get started
+					</div>
+				) : null}
+				{view == "a" && info.app_id != 0 ? (
+					<AchievementView
+						achievements={achievements}
 						icons={icons}
-            updateStatus={updateStatus}
-            loadAchievements={LoadAchievements}
-          />
-        ) : null}
-        {view == "s" && info.app_id != 0 ? (
-          <StatisticView
-            stats={stats}
-            updateStatus={updateStatus}
-						loadStatistics = {() => LoadStatistics(info.app_id)}
-          />
-        ) : null}
-      </div>
-    </>
-  );
+						updateStatus={updateStatus}
+						loadAchievements={LoadAchievements}
+					/>
+				) : null}
+				{view == "s" && info.app_id != 0 ? (
+					<StatisticView
+						stats={stats}
+						updateStatus={updateStatus}
+						loadStatistics={() => LoadStatistics(info.app_id)}
+					/>
+				) : null}
+			</div>
+		</>
+	);
 }
 
 export default App;
